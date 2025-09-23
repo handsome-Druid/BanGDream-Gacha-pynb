361c1bd7332f7897c5d0e35e3b0ae96bc46a79a0
山泥若叶睦 <102826168@qq.com>
2025-09-23 21:20:51 +0800

    update 1.2.1

--- bangdreamgacha_numba.py
+++ bangdreamgacha_numba.py
@@ -9,7 +9,7 @@ from numba import njit, prange
 STEP_5 = 0.005
 STEP_4 = 0.0075
 
-@njit(cache=False)
+@njit(cache=True)
 def simulate_one_round_numba(total_5, want_5, want_4, normal, rng_state):
     # rng_state: 128-bit xoroshiro like simple PRNG (we'll implement xorshift64*)
     def rand01(state):
