3da29cda676f32fb0fb740fd44d381fecbf9625a
handsome-Druid <102826168@qq.com>
2025-09-24 09:38:24 +0800

    update 1.3.1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,19 @@
-conda env create -f environment.yml
+# 1. 创建并激活环境
+if (-Not (Test-Path "C:\Users\$env:USERNAME\.conda\envs\gacha-env")) {
+    conda env create -f environment.yml -n gacha-env
+}
 conda activate gacha-env
-pyuic5 .\gacha_gui.ui -x -o .\Ui_gacha_gui.py
-pyrcc5 .\res.qrc -o .\res_rc.py
+
+
+# 2. 生成 UI
+if (Test-Path ".\gacha_gui.ui") {
+    pyuic5 .\gacha_gui.ui -x -o .\Ui_gacha_gui.py
+}
+
+# 3. 生成资源文件
+if (Test-Path ".\res.qrc") {
+    pyrcc5 .\res.qrc -o .\res_rc.py
+}
+
+# 4. 打包
 pyinstaller --clean .\release.spec
